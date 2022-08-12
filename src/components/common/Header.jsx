875042ce387e918e23862c8ace0f8127e8bb5e01
Changsoon-YUN <changsoonyun@naver.com>
2022-08-12 18:58:45 +0900

    Update/layout,header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,10 +4,55 @@ import { useNavigate } from "react-router-dom";
 function Header(props) {
     const navigate = useNavigate();
     return (
-        <div>
-
-        </div>
+        <StHeader>
+            <StLeftHeader>
+                <StLogo onClick={()=> navigate("/")}></StLogo>
+            </StLeftHeader>
+            <StRightHeader>
+              <StNav>
+                <StNavUl>
+                    <StNavLi onClick={()=> navigate("/mypage")}>마이페이지</StNavLi>
+                    <StNavLi>로그아웃</StNavLi>
+                </StNavUl>
+              </StNav>
+            </StRightHeader>
+        </StHeader>
     );
 };
+
+
+const StHeader = styled.header`
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const StLeftHeader = styled.div``
+
+const StLogo = styled.div`
+  background-image: url("/static/images/logo.webp");
+  background-repeat: no-repeat;
+  height: 41px;
+  width: 128px;
+  cursor: pointer
+`
+
+
+const StRightHeader = styled.div``
+
+
+const StNav = styled.nav``
+
+const StNavUl = styled.ul`
+  display: flex;
+  list-style: none;
+`
+
+const StNavLi = styled.li`
+  padding: 10px 20px;
+  margin: 0 10px;
+  cursor: pointer;
+`
 
 export default Header;
--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -3,10 +3,17 @@ import styled from "styled-components";
 function Layout(props) {
 
     return (
-        <div>
+        <StLayout>
             {props.children}
-        </div>
+        </StLayout>
     );
 };
+
+const StLayout = styled.div`
+    max-width: 1000px;
+    min-height: 100vh;
+    padding: 0 20px;
+    margin: 0 auto;
+`
 
 export default Layout;
